3c0e592b1b3bc073997ff700d2e926bb3199b619
Mahima Chouhan <mahimachouhanmahi@gmail.com>
2025-05-26 22:58:19 +0530

    frontend-url-added

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use("/api/resume", resumeRoutes);
 app.use("/uploads",
     express.static(path.join(__dirname, "uploads"), {
         setHeaders : (res, path) => {
-            res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+            res.set("Access-Control-Allow-Origin", "https://resume-builder-app-frontend.onrender.com");
         },
     })
 );
